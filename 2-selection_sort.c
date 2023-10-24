7372dd3a69acb2e70a7a53fae4bd7519a0a893bb
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-24 15:18:14 -0700

    selection sort 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,26 +6,31 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	int min, temp;
+	int min, temp, flag = 0;
 	size_t i, j, minIndex;
 
 	if (size < 2)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = array[i];
 		minIndex = i;
+		flag = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < min)
 			{
 				min = array[j];
 				minIndex = j;
+				flag = 1;
 			}
 		}
-		temp = array[i];
-		array[i] = min;
-		array[minIndex] = temp;
-		print_array(array, size);
+		if (flag == 1)
+		{
+			temp = array[i];
+			array[i] = min;
+			array[minIndex] = temp;
+			print_array(array, size);
+		}
 	}
 }
