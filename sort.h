fb8915da4ae0ddb5d76f5cc41980624ff3829e70
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-10-28 09:25:20 -0700

    quick sort lomuto

--- sort.h
+++ sort.h
@@ -21,4 +21,6 @@ void insertion_sort_list(listint_t **list);
 listint_t **swap(listint_t *node, listint_t *next_node, listint_t **list);
 int get_size(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_2(int *array, size_t size, int *orig_array, size_t orig_size);
 #endif
